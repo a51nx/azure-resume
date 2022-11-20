14889122c67f742ce007cd486385bd1a1d00cad3
a51nx <learn.hugh@outlook.com>
2022-11-20 20:57:44 +1300

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event => {
     getVisitCount();
 }))
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounter-hf.azurewebsites.net/api/GetResumeCounter?code=bONyzJU6NN07QHt2i0VJl78ncJAQlwlO9FFgYFcQwbAgAzFuDyG__g=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30; 
-    fetch(functionApi).then(response =>{
+    fetch(functionApiUrl).then(response =>{
         return response.json()
     }).then(response =>{
         console.log("Website called function API.")
